09c713ddb6100dba4b4cb2646a8bdaa5fa118ebd
Marc Daya <marc@sinistral.net>
2018-10-03 07:43:34 -0600

    Package using the boot-aws-lambda-kit.

---
+++ source/clj/hellolambda/boot.clj
@@ -0,0 +1,28 @@
+
+(ns hellolambda.boot
+  (:refer-clojure :exclude [test])
+  (:require [boot.core                   :as core]
+            [boot.task.built-in          :as task]
+            [adzerk.boot-cljs            :as cljs       :refer [cljs]]
+            [cemerick.piggieback         :as piggie     :refer [cljs-repl]]
+            [cljs.repl.node              :as cljs-node  :refer [repl-env]]
+            [crisptrutski.boot-cljs-test :as cljs-test  :refer [test-cljs]]
+            [boot-aws-lambda-kit.core    :as lambda-kit :refer [handler:cljs]]))
+
+(defn start-cljs-repl
+  []
+  (cljs-repl (repl-env)))
+
+(core/deftask test
+  []
+  (core/task-options! test-cljs {:js-env :node})
+  (test-cljs))
+
+(core/deftask build
+  []
+  (comp (cljs :ids #{"hellolambda"}
+              :optimizations :none
+              :compiler-options {:target :nodejs})
+        (handler:cljs :ids  #{"hellolambda"})
+        (task/jar :file "hellolambda.zip")
+        (task/target :dir #{"target"})))
